db000bb1a71f33d05edb49960f065b1b5787a582
chenhs <617950737@qq.com>
2021-03-13 22:14:25 +0800

    Signed-off-by: chenhs <617950737@qq.com>

--- src/main/java/com/example/securityDemo/SwaggerConfig.java
+++ src/main/java/com/example/securityDemo/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.example.securityDemo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,6 +14,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwaggerBootstrapUI
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
